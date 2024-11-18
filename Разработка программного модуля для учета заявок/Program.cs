607a0ac34635fa77e4b83b36e03a97472bca7695
Pinkar282 <137715096+Pinkar282@users.noreply.github.com>
2024-11-19 00:44:17 +0500

    Пункт 2.2

--- Разработка программного модуля для учета заявок/Program.cs
+++ Разработка программного модуля для учета заявок/Program.cs
@@ -4,7 +4,7 @@ var app = builder.Build();
 //Список заявок(База данных)
 List<Order> repo = 
 [
-    new (1, 2, 5, 2013, "Компьютер", "Отвал чипа", "да", "А.М.Антонов", "в ожидании"),
+    new (1, 2, 5, 2013, "Компьютер", "Отвал чипа", "да", "А.М.Антонов", "в ожидании", "Олег"),
 ];
 
 //Get- отвечает за получение(допустим "app.MapGet("/", () => repo);" получает информацию из репозитория(repo) и выводит ее на сайте) 
@@ -13,10 +13,22 @@ app.MapGet("/", () => repo);
 //Post- отвечает за влияние извне(пользователь)
 app.MapPost("/", (Order zaiavka) => repo.Add(zaiavka));
 
+//Put- отвечает
+app.MapPut("/{number}", (int number, OrderUpdateDTO dto) => //Приходит объект OrderUpdateDTO
+{
+    Order buffer = repo.Find(x => x.Number == number); // Номер заявки 
+    if (buffer == null) 
+        return Results.NotFound("...");
+    buffer.Status = dto.Status;
+    buffer.Master = dto.Master;
+    buffer.Description = dto.Description;
+    return Results.Json(buffer);
+});
 //Run- запускает приложение
 app.Run();
 
-
+//DTO- передает в MapPut 3 поля: 
+record class OrderUpdateDTO(string Status, string Description, string Master);
 
 
 class Order
@@ -27,12 +39,12 @@ class Order
     int year;
     string device;
     string problemType;
-    string descriprion;
+    string description;
     string client;
     string status;
+    string master; //ответственный за работу
 
-
-    public Order(int number, int day, int mouth, int year, string device, string problemType, string descriprion, string client, string status)
+    public Order(int number, int day, int mouth, int year, string device, string problemType, string description, string client, string status, string master)
     {
         Number = number;
         Day = day;
@@ -40,9 +52,10 @@ class Order
         Year = year;
         Device = device;
         ProblemType = problemType;
-        Descriprion = descriprion;
+        Description = description;
         Client = client;
         Status = status;
+        Master = master;
     }
 
     public int Number {get; set;}
@@ -51,7 +64,8 @@ class Order
     public int Year {get; set;}
     public string Device {get; set;}
     public string ProblemType {get; set;}
-    public string Descriprion {get; set;}
+    public string Description {get; set;}
     public string Client {get; set;}
     public string Status {get; set;}
+    public string Master { get; set; }
 }
